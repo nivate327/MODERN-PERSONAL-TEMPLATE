fa1e43e0f54ab1bc01360cf201e87fe19daf19be
nivate327 <rohannivate321@gmail.com>
2021-07-13 12:54:50 +0530

    second commit

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+let email=document.getElementById("exampleInputEmail1");
+
+let phone=document.getElementById("exampleInputPassword1");
+
+let submit=document.getElementById("submit");
+
+
+submit.addEventListener("click", ()=>
+{
+  console.log(email.value);
+  alert(`Your Email ${email.value} and Phone Number is ${phone.value}`);
+})
+
+
+
